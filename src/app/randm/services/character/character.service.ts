02802cbb4f3e30a5ba530727cadc6b57aaea5a39
Efren Galarza <efrenpgc2602@gmail.com>
2023-06-13 17:15:29 -0500

    feat(app-randm): Agregar paginación a la pagina Character y derivados

--- src/app/randm/services/character/character.service.ts
+++ src/app/randm/services/character/character.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Root, Character } from '../dtos/character.dto';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/env.dev';
 
 @Injectable({ providedIn: 'root' })
 export class CharacterService {
   constructor(private httpClient: HttpClient) {}
+
+  getCharactersPage(uri: string) {
+    return this.httpClient.get<Root>(`${environment.API_RANDM_CHARACTER}/?${uri}`);
+  }
 
   getAllCharacters(): Observable<Root> {
     return this.httpClient.get<Root>(environment.API_RANDM_CHARACTER);
