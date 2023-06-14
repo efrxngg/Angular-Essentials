045f4deaed28e809d8d7b0d8343d9b08c4603c2e
Efren Galarza <efrenpgc2602@gmail.com>
2023-06-14 14:10:41 -0500

    feat(app-randm): Adaptar proyecto para múltiples entornos
    - Se eliminio el env.dev.ts, para seguir el estandar definido por el Framework para el manejo de multiples entornos

--- src/app/randm/services/character/character.service.ts
+++ src/app/randm/services/character/character.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Root, Character } from '../dtos/character.dto';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/env.dev';
+import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class CharacterService {
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    API_RANDM_CHARACTER: 'https://rickandmortyapi.com/api/characterr',
+  };
+  
